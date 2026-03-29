60c776609c21e71000d7a8ed46d5e6b789d39f91
TOTVSGustavoFerreira <carlos.ferreira@totvs.com.br>
2026-03-29 11:57:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("🏢 Portal de Extratores de Folha")
 st.write("Bem-vindo ao portal de processamento e conversão de folhas de pagamento!")
 st.write("👈 **Selecione a ferramenta desejada no menu lateral à esquerda:**")
 
-st.info("📊 **1. Extrator Excel:** Extrai dados de folhas em formato .xlsx e .xls.")
+st.info("📊 **1. Extrator Excel:** Extrai dados de folhas (FORTES) em formato .xlsx e .xls.")
 st.info("📄 **2. Extrator PDF:** Extrai dados APENAS FOLHAS nativas geradas (IOB) em .pdf.")
 
 st.write("---")
